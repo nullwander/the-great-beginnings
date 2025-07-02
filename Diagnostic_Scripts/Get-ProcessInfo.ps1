5baa6f896e5f7994dd364c459c94e016eea05304
nullwander <nullwander@outlook.com>
2025-07-02 11:45:05 -0600

    Update Get-ProcessInfo.ps1

--- Diagnostic_Scripts/Get-ProcessInfo.ps1
+++ Diagnostic_Scripts/Get-ProcessInfo.ps1
@@ -2,13 +2,13 @@
 $output = @()
 $output += "=== Process Info: $(Get-Date) ==="
 
-$output += (Get-Process | Group-Object Name | ForEach-Object {  #Grabs processes and group names them. 
+$output += (Get-Process | Group-Object Name | ForEach-Object {  #Grabs processes
     [PSCustomObject]@{
         Name = $_.Name
         TotalCPU = ($_.Group | Measure-Object CPU -Sum).Sum
         TotalMemoryMB = [math]::Round(($_.Group | Measure-Object WorkingSet -Sum).Sum / 1MB, 2)
         Count = $_.Count
-    } # this block takes the objects from Get-Process, calcs their sum for the groups on both CPU and Memory in MB. Then counts to organize. 
+    } # this block takes the objects from Get-Process, calcs their sum for the groups on both CPU and Memory in MB, then counts to organize. 
 } | Sort-Object TotalCPU -Descending | Select-Object -First 5 ) # Sorts for more readability. Appends to file. 
 
 $output += "=== End of Process Info ===" 
